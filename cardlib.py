f28d83c91cca71e60db5dc2c8f0c3ee48a62e590
Callum Shipton <callumshipton@msn.com>
2018-07-05 19:21:00 +0100

    -Fixed bug in init code where strings where being compared with ints. Now all numbers are strings and all values are numbers. -Cleaned up initialisation code to compute all card possibilities
    Signed-off-by: Callum Shipton <callumshipton@msn.com>

--- cardlib.py
+++ cardlib.py
@@ -1,4 +1,5 @@
 import random
+import itertools
 
 class Card:
 	def __init__(self, number, suit):
@@ -10,6 +11,7 @@ class Card:
 		elif number == 'Jack': self.value = 11
 		elif number == 'Queen': self.value = 12
 		elif number == 'King': self.value = 13
+		else: self.value = int(number)
 	
 	def compare(self, card):
 		if self.value > card.value: return 1
@@ -20,8 +22,13 @@ class Card:
 		return self.number + " of " + self.suit + "s"
 	
 class Deck:
+	cards = []
 	def __init__(self):
-		self.cards = [Card('Ace', 'Spade'), Card('2', 'Spade'), Card('3', 'Spade'), Card('4', 'Spade'), Card('5', 'Spade'), Card('6', 'Spade'), Card('7', 'Spade'), Card('8', 'Spade'), Card('9', 'Spade'), Card('10', 'Spade'), Card('Jack', 'Spade'), Card('Queen', 'Spade'), Card('King', 'Spade'), Card('Ace', 'Diamond'), Card('2', 'Diamond'), Card('3', 'Diamond'), Card('4', 'Diamond'), Card('5', 'Diamond'), Card('6', 'Diamond'), Card('7', 'Diamond'), Card('8', 'Diamond'), Card('9', 'Diamond'), Card('10', 'Diamond'), Card('Jack', 'Diamond'), Card('Queen', 'Diamond'), Card('King', 'Diamond'), Card('Ace', 'Club'), Card('2', 'Club'), Card('3', 'Club'), Card('4', 'Club'), Card('5', 'Club'), Card('6', 'Club'), Card('7', 'Club'), Card('8', 'Club'), Card('9', 'Club'), Card('10', 'Club'), Card('Jack', 'Club'), Card('Queen', 'Club'), Card('King', 'Club'), Card('Ace', 'Heart'), Card('2', 'Heart'), Card('3', 'Heart'), Card('4', 'Heart'), Card('5', 'Heart'), Card('6', 'Heart'), Card('7', 'Heart'), Card('8', 'Heart'), Card('9', 'Heart'), Card('10', 'Heart'), Card('Jack', 'Heart'), Card('Queen', 'Heart'), Card('King', 'Heart')]
+		values = ['Ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King']
+		suits = ['Spade', 'Diamond', 'Club', 'Heart']
+		allCards = itertools.product(values, suits)
+		for number,suit in allCards:
+			self.cards.append(Card(number, suit))
 	
 	def shuffle(self):
 		print("Shuffling")
